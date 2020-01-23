d01dc80899febbea5746634461c04a3bf18aca7d
annaaleksanyan <anna.aleksanyan1.y@tumo.org>
2020-01-23 18:46:53 +0400

    lesson3 homework

--- index.js
+++ index.js
@@ -1,43 +1,17 @@
-var express = require("express");
-var app = express();
+var express = require("express"); 
+var app = express(); 
 
-app.get("/name/:name", function(req, res){
-   var name = req.params.name;
-   res.send("<h1>Hello " + name + "</h1>");
-   res.redirect('http://google.com');
+app.get("/", function (req, res) { 
+var urlGoogle = 'https://www.google.com/' 
+res.redirect(urlGoogle) 
 });
-app.get('/url', function(req, res){
-   var url = 'http://google.com'
-   res.redirect(301, url)
- });
- app.get('*', function(req, res) {
-   res.send('what???', 404);
+app.get("/:", function (req, res) {
+var urlSearch = 'https://google.com/search?q=search value' 
+res.redirect(urlSearch) 
 });
-app.search('/url', function(req, res){
-   var url = 'google.com/search?q=search value'
-   res.redirect(301, url)
+app.get("/*", function (req, res) { 
+res.send("<h1>404 Error");
 });
-
-
-
-
-// app.get("/name/:name", function(req, res){
-//    var name = req.params.name;
-//    res.send("<h1>Hello " + name + "</h1>");
-//    res.redirect('http://google.com');
-// });
-// app.listen(3000, function(){
-//    console.log("Example is running on port 3000");
-// });
-// app.search(function(req, res){
-//    res.redirect('google.com/search?q=search value')
-// })
-// app.error('/*',function(req, res){ 
-// }
-
-
-// // // request(app)
-// // if (error.response.status == 404) {
-// //    res.redirect("/404")
-// // }
-// // return res.redirect('/404');
+app.listen(3000, function () {
+console.log("Example is running on port 3000");
+});
